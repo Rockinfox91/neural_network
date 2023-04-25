4e690205f26621217a64a602a7c5d7bc9fd62e79
Lucas BOISTAY <lucasboistay@hotmail.fr>
2023-04-25 02:15:00 +0200

    Little changes

--- src/node/node.py
+++ src/node/node.py
@@ -1,18 +1,18 @@
 class Node:
-    total_node = 0
+    __total_node = 0
 
-    def __init__(self, n_in: int, n_out: int):
+    def __init__(self, n_in: int, n_out: int) -> None:
         assert isinstance(n_in, int) and n_in >= 0, "n_in should be a non-negative integer"
         assert isinstance(n_out, int) and n_out >= 0, "n_out should be a non-negative integer"
 
-        self.id = Node.total_node
-        Node.total_node += 1
+        self.id = Node.__total_node
+        Node.__total_node += 1
 
-        self.input = n_in
-        self.output = n_out
+        self.n_input = n_in
+        self.n_output = n_out
 
-    def __str__(self):
-        return f"Node {str(self.id)}: ({str(self.input)},{str(self.output)})"
+    def __str__(self) -> str:
+        return f"Node {str(self.id)}: ({str(self.n_input)},{str(self.n_output)})"
 
 
 if __name__ == '__main__':
--- tests/test_node.py
+++ tests/test_node.py
@@ -4,6 +4,10 @@ from src.node.node import Node
 
 class TestNode(unittest.TestCase):
 
+    # Test privacy of Node.__total_node
+    def test_total_node_privacy(self):
+        with self.assertRaises(AttributeError):
+            self.assertEqual(Node.total_node, 0)
     # Create a node Errors
     def test_node_id(self):
         node1 = Node(1, 2)
@@ -13,8 +17,8 @@ class TestNode(unittest.TestCase):
 
     def test_valid_input_output(self):
         node = Node(3, 4)
-        self.assertEqual(node.input, 3)
-        self.assertEqual(node.output, 4)
+        self.assertEqual(node.n_input, 3)
+        self.assertEqual(node.n_output, 4)
 
     def test_invalid_input_type(self):
         with self.assertRaises(AssertionError):
@@ -34,13 +38,13 @@ class TestNode(unittest.TestCase):
 
     def test_zero_input_output(self):
         node = Node(0, 0)
-        self.assertEqual(node.input, 0)
-        self.assertEqual(node.output, 0)
+        self.assertEqual(node.n_input, 0)
+        self.assertEqual(node.n_output, 0)
 
     # Print a node Errors
     def test_print_node(self):
         node = Node(1, 2)
-        self.assertEqual(str(node), f"Node {node.id}: ({node.input},{node.output})")
+        self.assertEqual(str(node), f"Node {node.id}: ({node.n_input},{node.n_output})")
 
 
 if __name__ == '__main__':
